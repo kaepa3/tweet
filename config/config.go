175c0ba4f3c13b15f21a89cf6ff11a56a3fd47ae
kaepa3 <kaepa3@gmail.com>
2020-07-05 07:54:44 +0000

    add basic

---
+++ config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/BurntSushi/toml"
+)
+
+// Config this app config
+type TwitterConfig struct {
+	ApiKey            string
+	ApiKeySecret      string
+	AccessToken       string
+	AccessTokenSecret string
+}
+
+func ReadConfig(path string) (*TwitterConfig, bool) {
+	var config TwitterConfig
+	_, err := toml.DecodeFile(path, &config)
+	if err != nil {
+		return nil, false
+	}
+	return &config, true
+}
